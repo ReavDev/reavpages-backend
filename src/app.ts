bc1d044652776e63ef116a265b5b3394a2942cb6
Prudent Bird <danielwari.me@gmail.com>
2024-09-17 13:40:23 +0100

    chore: added typecheck for PORT env

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,10 @@ app.get("/", (res: Response) => {
   res.send("Welcome to ReavPages Backend server");
 });
 
+if (process.env["PORT"] === undefined) {
+  throw new Error("Port Environment Variable is undefined");
+}
+
 app.listen(process.env["PORT"], () => {
   console.log(`Server is running at http://localhost:${process.env["PORT"]}`);
 });
